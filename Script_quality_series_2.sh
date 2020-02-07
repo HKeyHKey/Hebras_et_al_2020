947e2fbab972b47303782f2ea371f83b604dfcf1
Hervé Seitz <herve.seitz@igh.cnrs.fr>
2020-02-07 16:21:24 +0100

    Delete Script_quality_series_2.sh

--- Script_quality_series_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-for f in `cat $1`;do name=`echo $f | sed -e 's|Amplicon_reads_from_Mapping_default_parameters_||' -e 's|\.bed||'`;./Module_converts_quality_scores.pl $name $f;Rscript R_display_quality $name;done
-
